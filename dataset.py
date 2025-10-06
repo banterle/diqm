c464ce19d7cf06640d1fca98b218f264b6ce8ac2
banterle <frabante@gmail.com>
2025-10-06 11:58:22 +0200

    fix histograms

--- dataset.py
+++ dataset.py
@@ -115,19 +115,19 @@ def split_data(data_dir, random_state=42, group=None, groupaffine= 1):
     #
     #
     #
-    q_tra, h_tra = getVec(train)
-    q_val, h_val = getVec(val)
-    q_tes, h_tes = getVec(test)
+    q_tra, h_tra  = getVec(train)
+    q_val, h_val  = getVec(val)
+    q_test, h_test = getVec(test)
 
     plt.clf()
-    sns.distplot(q_tra, kde=True, rug=True, bins=100)
-    plt.savefig('hist_q_train.png')
+    sns.displot(q_tra, kde=True, rug=True, bins=100)
+    plt.savefig(os.path.join(data_dir, 'hist_train.png'))
     plt.clf()
-    sns.distplot(q_val, kde=True, rug=True, bins=100)
-    plt.savefig('hist_q_val.png')
+    sns.displot(q_val, kde=True, rug=True, bins=100)
+    plt.savefig(os.path.join(data_dir, 'hist_val.png'))
     plt.clf()
-    sns.distplot(q_tes, kde=True, rug=True, bins=100)
-    plt.savefig('hist_q_test.png')
+    sns.displot(q_test, kde=True, rug=True, bins=100)
+    plt.savefig(os.path.join(data_dir, 'hist_test.png'))
 
     return train, val, test
 
